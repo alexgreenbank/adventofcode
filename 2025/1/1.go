6fdddcd88a1255119dae7873860a60cb0e2e07e5
Alex Greenbank <alex@greenbank.org>
2026-03-25 18:03:49 +0000

    AoC 2025 Day 1 part 2 in golang
    Signed-off-by: Alex Greenbank <alex@greenbank.org>

--- 2025/1/1.go
+++ 2025/1/1.go
@@ -11,19 +11,42 @@ import (
 )
 
 // Perform a move from current dial position
-// Returns new dial position and p1 score increase
-func doMove(curr int, move string) (int, int) {
+// Returns new dial position and p1 and p2 score increase
+func doMove(curr int, move string) (int, int, int) {
 	p1 := 0
+	p2 := 0
+
+	// Ensure inputs are valid
+	if curr < 0 || curr >= 100 {
+		log.Fatal("Invalid curr value in doMove, must be [0,99]")
+	}
+	if len(move) < 2 {
+		log.Fatal("Invalid move string in doMove")
+	}
+	if move[0] != 'L' && move[0] != 'R' {
+		log.Fatal("Invalid move letter in doMove")
+	}
 
 	var n int
 	n, err := strconv.Atoi(move[1:])
 	if err != nil {
 		log.Fatal(err)
 	}
+	if n < 0 {
+		log.Fatalf("Does not handle negative n values in input [%s]", move)
+	}
+	// We add on any multiples of 100 to the p2inc score
+	p2 += int(n/100)
+	// We can now strip n down to the range [0,99]
+	n %= 100
 	if move[0] == 'L' {
 		n = 0-n
-	} else if move[0] != 'R' {
-		log.Fatalf("FATAL: instruction does not begin with either 'L' or 'R' [%s]", move)
+	}
+	if (curr > 0 && curr+n < 0) || (curr+n > 100 ) {
+		// This move crosses over the 0 ticker
+		// We handle the case of it ending on 0 later
+		// We ignore the case of it starting on 0 and ending -ve as it hasn't passed 0 on this move
+		p2++
 	}
 	curr += n
 	// % on negative numbers will produce a negative number
@@ -35,8 +58,13 @@ func doMove(curr int, move string) (int, int) {
 	// fmt.Printf("AFTER [%s] dial = %d\n", str, curr)
 	if curr == 0 {
 		p1++
+		if n != 0 {
+			// If we did an R100 starting on 0 then we've counted the 0 for part2 already above
+			// so don't double count it here
+			p2++
+		}
 	}
-	return curr, p1
+	return curr, p1, p2
 }
 
 func main() {
@@ -51,14 +79,16 @@ func main() {
 	defer file.Close()
 
 	p1 := 0
+	p2 := 0
 	curr := 50
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		str := scanner.Text()
-		newDial, p1inc := doMove(curr, str)
+		newDial, p1inc, p2inc := doMove(curr, str)
 
 		p1 += p1inc
+		p2 += p2inc
 		curr = newDial
 	}
 
@@ -67,6 +97,7 @@ func main() {
 	}
 
 	fmt.Printf("part1: %d\n", p1)
+	fmt.Printf("part2: %d\n", p2)
 	fmt.Printf("dur=%s\n", time.Since(start))
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
--- 2025/1/1_test.go
+++ 2025/1/1_test.go
@@ -12,26 +12,43 @@ func Test_doMove(t *testing.T) {
 		move string
 		expectedDial int
 		expectedP1 int
+		expectedP2 int
 	}{
-		{"R1 from 50", 50, "R1", 51, 0},
-		{"L1 from 50", 50, "L1", 49, 0},
-		{"R49 from 50", 50, "R49", 99, 0},
-		{"L49 from 50", 50, "L49", 1, 0},
-		{"R50 from 50", 50, "R50", 0, 1},
-		{"L50 from 50", 50, "L50", 0, 1},
-		{"R51 from 50", 50, "R51", 1, 0},
-		{"L51 from 50", 50, "L51", 99, 0},
-		{"R150 from 50", 50, "R150", 0, 1},
-		{"L150 from 50", 50, "L150", 0, 1},
-		{"R151 from 50", 50, "R151", 1, 0},
-		{"L151 from 50", 50, "L151", 99, 0},
+		// Simple cases
+		{"R1 from 50", 50, "R1", 51, 0, 0},
+		{"L1 from 50", 50, "L1", 49, 0, 0},
+		{"R49 from 50", 50, "R49", 99, 0, 0},
+		{"L49 from 50", 50, "L49", 1, 0, 0},
+		// Ending on 0
+		{"R50 from 50", 50, "R50", 0, 1, 1},
+		{"L50 from 50", 50, "L50", 0, 1, 1},
+		// Passing 0
+		{"R51 from 50", 50, "R51", 1, 0, 1},
+		{"L51 from 50", 50, "L51", 99, 0, 1},
+		// Ending on 0 with an extra full spin
+		{"R150 from 50", 50, "R150", 0, 1, 2},
+		{"L150 from 50", 50, "L150", 0, 1, 2},
+		// Passing 0 with an extra full spin
+		{"R151 from 50", 50, "R151", 1, 0, 2},
+		{"L151 from 50", 50, "L151", 99, 0, 2},
+		// Let's test some edge cases starting on 0
+		{"R1 from 0", 0, "R1", 1, 0, 0},
+		{"L1 from 0", 0, "L1", 99, 0, 0},
+		{"R100 from 0", 0, "R100", 0, 1, 1},
+		{"L100 from 0", 0, "L100", 0, 1, 1},
+		{"R200 from 0", 0, "R200", 0, 1, 2},
+		{"L200 from 0", 0, "L200", 0, 1, 2},
+		// {"Fatal", -1, "C100", 99, 0, 2},
+		// {"Fatal", 50, "C", 99, 0, 2},
+		// {"Fatal", 50, "C100", 99, 0, 2},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.testName, func(t *testing.T) {
-			dial, p1inc := doMove(tt.curr, tt.move)
-			require.Equal(t, dial, tt.expectedDial)
-			require.Equal(t, p1inc, tt.expectedP1)
+			dial, p1inc, p2inc := doMove(tt.curr, tt.move)
+			require.Equal(t, tt.expectedDial, dial)
+			require.Equal(t, tt.expectedP1, p1inc)
+			require.Equal(t, tt.expectedP2, p2inc)
 		})
 	}
 
